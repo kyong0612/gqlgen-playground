dbde3a5fcd87bfa4958ad4f7eafcb199c94fe503
kimkiyong <fenderst0612@gmail.com>
2023-07-18 22:52:33 +0900

    feat: create root database

--- tools.go
+++ tools.go
@@ -3,4 +3,8 @@
 
 package tools
 
-import _ "github.com/99designs/gqlgen"
+import (
+	_ "github.com/99designs/gqlgen"
+	_ "github.com/volatiletech/sqlboiler/v4"
+	_ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql"
+)
