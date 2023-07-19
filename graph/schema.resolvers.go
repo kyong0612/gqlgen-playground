9bbd5af2d53a9b57294bf1c2000cfdb7b9b4f6f2
kimkiyong <fenderst0612@gmail.com>
2023-07-19 22:07:24 +0900

    feat: split query

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -15,6 +15,11 @@ func (r *mutationResolver) AddProjectV2ItemByID(ctx context.Context, input gql.A
 	panic(fmt.Errorf("not implemented: AddProjectV2ItemByID - addProjectV2ItemById"))
 }
 
+// Owner is the resolver for the owner field.
+func (r *projectV2Resolver) Owner(ctx context.Context, obj *gql.ProjectV2) (*gql.User, error) {
+	panic(fmt.Errorf("not implemented: Owner - owner"))
+}
+
 // Repository is the resolver for the repository field.
 func (r *queryResolver) Repository(ctx context.Context, name string, owner string) (*gql.Repository, error) {
 	panic(fmt.Errorf("not implemented: Repository - repository"))
@@ -30,11 +35,44 @@ func (r *queryResolver) Node(ctx context.Context, id string) (gql.Node, error) {
 	panic(fmt.Errorf("not implemented: Node - node"))
 }
 
+// Owner is the resolver for the owner field.
+func (r *repositoryResolver) Owner(ctx context.Context, obj *gql.Repository) (*gql.User, error) {
+	panic(fmt.Errorf("not implemented: Owner - owner"))
+}
+
+// Issue is the resolver for the issue field.
+func (r *repositoryResolver) Issue(ctx context.Context, obj *gql.Repository, number int) (*gql.Issue, error) {
+	panic(fmt.Errorf("not implemented: Issue - issue"))
+}
+
+// Issues is the resolver for the issues field.
+func (r *repositoryResolver) Issues(ctx context.Context, obj *gql.Repository, after *string, before *string, first *int, last *int) (*gql.IssueConnection, error) {
+	panic(fmt.Errorf("not implemented: Issues - issues"))
+}
+
+// PullRequest is the resolver for the pullRequest field.
+func (r *repositoryResolver) PullRequest(ctx context.Context, obj *gql.Repository, number int) (*gql.PullRequest, error) {
+	panic(fmt.Errorf("not implemented: PullRequest - pullRequest"))
+}
+
+// PullRequests is the resolver for the pullRequests field.
+func (r *repositoryResolver) PullRequests(ctx context.Context, obj *gql.Repository, after *string, before *string, first *int, last *int) (*gql.PullRequestConnection, error) {
+	panic(fmt.Errorf("not implemented: PullRequests - pullRequests"))
+}
+
 // Mutation returns gql.MutationResolver implementation.
 func (r *Resolver) Mutation() gql.MutationResolver { return &mutationResolver{r} }
+
+// ProjectV2 returns gql.ProjectV2Resolver implementation.
+func (r *Resolver) ProjectV2() gql.ProjectV2Resolver { return &projectV2Resolver{r} }
 
 // Query returns gql.QueryResolver implementation.
 func (r *Resolver) Query() gql.QueryResolver { return &queryResolver{r} }
 
+// Repository returns gql.RepositoryResolver implementation.
+func (r *Resolver) Repository() gql.RepositoryResolver { return &repositoryResolver{r} }
+
 type mutationResolver struct{ *Resolver }
+type projectV2Resolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type repositoryResolver struct{ *Resolver }
